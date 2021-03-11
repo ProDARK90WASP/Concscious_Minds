b98fe9036afeb201e55ec544da02aaa56a386efa
ProDARK90WASP <73740252+ProDARK90WASP@users.noreply.github.com>
2021-03-11 11:53:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function calcBMI() {
     var weight = document.bmiform.pounds.value, height = document.bmiform.inches.value;
-    document.bmiform.bmi.value = parseInt((weight * 703) / (height * height));
+    //document.bmiform.bmi.value = parseInt((weight * 703) / (height * height));
+    document.bmiform.bmi.value = parseInt((weight) / ((height/100) * (height/100)));
   } 
